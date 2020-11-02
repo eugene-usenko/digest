2e228d34efbe45f5c288e06fb069fbd594b21f93
iusenko <zhenya.usenko@gmail.com>
2020-11-03 00:45:52 +0200

    Configure deps & fluent ui

---
+++ src/components/theme.js
@@ -0,0 +1,32 @@
+/**
+ * Primary color:    #0076A8
+ * Text color:       #323130
+ * Background color: #ffffff
+ * @see https://fabricweb.z5.web.core.windows.net/pr-deploy-site/refs/heads/master/theming-designer/index.html
+ */
+export default Object.freeze({
+  palette: {
+    themePrimary: '#0076a8',
+    themeLighterAlt: '#f1f9fc',
+    themeLighter: '#cbe6f1',
+    themeLight: '#a0d0e5',
+    themeTertiary: '#51a6cb',
+    themeSecondary: '#1584b3',
+    themeDarkAlt: '#006a97',
+    themeDark: '#005a80',
+    themeDarker: '#00425e',
+    neutralLighterAlt: '#faf9f8',
+    neutralLighter: '#f3f2f1',
+    neutralLight: '#edebe9',
+    neutralQuaternaryAlt: '#e1dfdd',
+    neutralQuaternary: '#d0d0d0',
+    neutralTertiaryAlt: '#c8c6c4',
+    neutralTertiary: '#a19f9d',
+    neutralSecondary: '#605e5c',
+    neutralPrimaryAlt: '#3b3a39',
+    neutralPrimary: '#323130',
+    neutralDark: '#201f1e',
+    black: '#000000',
+    white: '#ffffff'
+  }
+})
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React from 'react'
+import { Link, graphql } from 'gatsby'
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Bio from '../components/bio'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import { initializeIcons, loadTheme } from '@fluentui/react'
+import theme from '../components/theme'
+
+initializeIcons()
+loadTheme(theme)
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
@@ -12,12 +17,11 @@ const BlogIndex = ({ data, location }) => {
   if (posts.length === 0) {
     return (
       <Layout location={location} title={siteTitle}>
-        <SEO title="All posts" />
+        <SEO title='All posts' />
         <Bio />
         <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
+          No blog posts found. Add markdown posts to "content/blog" (or the directory you specified for the
+          "gatsby-source-filesystem" plugin in gatsby-config.js).
         </p>
       </Layout>
     )
@@ -25,7 +29,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
+      <SEO title='All posts' />
       <Bio />
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
@@ -33,15 +37,11 @@ const BlogIndex = ({ data, location }) => {
 
           return (
             <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
+              <article className='post-list-item' itemScope itemType='http://schema.org/Article'>
                 <header>
                   <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
+                    <Link to={post.fields.slug} itemProp='url'>
+                      <span itemProp='headline'>{title}</span>
                     </Link>
                   </h2>
                   <small>{post.frontmatter.date}</small>
@@ -49,9 +49,9 @@ const BlogIndex = ({ data, location }) => {
                 <section>
                   <p
                     dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
+                      __html: post.frontmatter.description || post.excerpt
                     }}
-                    itemProp="description"
+                    itemProp='description'
                   />
                 </section>
               </article>
